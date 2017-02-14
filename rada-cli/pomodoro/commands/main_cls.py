306190abe3f99d2bbc6023a0b8ed17b98776cd5f
PaulUpendo <lovepaul46@outlook.com>
2017-02-14 16:36:35 +0300

    Ensured timestamp runs

--- rada-cli/pomodoro/commands/main_cls.py
+++ rada-cli/pomodoro/commands/main_cls.py
@@ -1,11 +1,22 @@
-import datetime
+from datetime import datetime
 
 
 class Pomo(object):
-    def __init__(self, shortbreak=5, task_dur=25, long_break=15, sound=True ):
-        self.shortbreak = shortbreak
+    time_val = []
+
+    def __init__(self, short_break=5, task_dur=25, long_break=15, sound=True ):
+        self.short_break = short_break
         self.task_dur = task_dur
         self.long_break = long_break
         self.sound = sound
+        self.day_today = datetime.now().strftime('%H'+':'+'%M'+':'+'%S')
+        self.time_val = [self.day_today]
+        print self.time_val
+
+
+
+
+
+
 
 
