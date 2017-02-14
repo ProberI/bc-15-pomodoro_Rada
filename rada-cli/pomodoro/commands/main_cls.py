b8fdb7d8a75fd57c74130acf543ca87b317898c8
PaulUpendo <lovepaul46@outlook.com>
2017-02-14 17:23:33 +0300

    solved class import bug

--- rada-cli/pomodoro/commands/main_cls.py
+++ rada-cli/pomodoro/commands/main_cls.py
@@ -1,17 +1,41 @@
 from datetime import datetime
-
+from os import system
+import time
 
 class Pomo(object):
     time_val = []
 
-    def __init__(self, short_break=5, task_dur=25, long_break=15, sound=True ):
+    def __init__(self, short_break=5, task_dur=25, long_break=15, sound=True, title=''):
         self.short_break = short_break
         self.task_dur = task_dur
         self.long_break = long_break
         self.sound = sound
         self.day_today = datetime.now().strftime('%H'+':'+'%M'+':'+'%S')
         self.time_val = [self.day_today]
+        self.stop = False
+        self.title = title
         print self.time_val
+
+    def timer(self, minutes):
+        while minutes:
+            if self.stop is True:
+                break
+            mins, secs = divmod(minutes, 60)
+            display_format = '{:02d}:{:02d}'.format(mins, secs)
+            clear()
+            print 'task:' + self.title
+            time.sleep(1)
+            minutes -= 1
+
+
+def clear():
+        system('Clear')
+
+Pomo()
+
+
+
+
 
 
 
---
+++ rada-cli/pomodoro/commands/start.py
@@ -0,0 +1,12 @@
+from base import Base
+from main_cls import Pomo
+
+
+class start(Base, Pomo):
+    def run(self, title=[]):
+        self.title = title
+
+
+
+
+
