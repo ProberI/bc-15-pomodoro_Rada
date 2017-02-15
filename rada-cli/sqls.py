dae8a4707d97468227195f10e947f3416ea45ca6
PaulUpendo <lovepaul46@outlook.com>
2017-02-16 02:45:54 +0300

    Create tasklists.db using engine

--- rada-cli/sqls.py
+++ rada-cli/sqls.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -10,6 +10,16 @@ Base = declarative_base()
 class Tasks(Base):
     __tablename__ = 'logs' # create table with name logs
     id = Column(Integer, primary_key=True, autoincrement=True)
-    
+    day = Column(DateTime())
+    task_name = Column(String(250))
+    task_cycles = Column(Integer)
+
+engine = create_engine("sqlite:///C:/Users/SIMPOL PAUL/Desktop/bc-15-pomodoro_Rada/rada-cli/tasklists.db")
+
+# Create all table
+Base.metadata.create_all(engine)
+
+
+
 
 
